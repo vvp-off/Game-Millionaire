29aa3d1bd04eecc5ba52c09c3a40cfbcb7982709
vvp-off <reservvp@gmail.com>
2025-07-19 17:20:03 +0300

    Add files via upload

---
+++ Millionaire/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by VP on 19.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
