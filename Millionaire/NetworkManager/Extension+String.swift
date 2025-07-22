9b4f7192778e68aac9c83594e75b9fe04557375a
marat_FMK <kosjachok8@yandex.ru>
2025-07-22 18:19:33 +0300

    extension String
    add extension for html decoded

---
+++ Millionaire/NetworkManager/Extension+String.swift
@@ -0,0 +1,22 @@
+//
+//  Extension+String.swift
+//  Millionaire
+//
+//  Created by Marat Fakhrizhanov on 22.07.2025.
+//
+
+import Foundation
+
+extension String {
+    var htmlDecoded: String {
+        guard let data = self.data(using: .utf8) else { return self }
+
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html,
+            NSAttributedString.DocumentReadingOptionKey.characterEncoding: String.Encoding.utf8.rawValue
+        ]
+
+        let decoded = try? NSAttributedString(data: data, options: options, documentAttributes: nil)
+        return decoded?.string ?? self
+    }
+}
