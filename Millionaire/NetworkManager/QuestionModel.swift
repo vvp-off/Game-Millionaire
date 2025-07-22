83052f5eec6e7402c9b1f5f5860383df2fab0bbd
marat_FMK <kosjachok8@yandex.ru>
2025-07-22 15:54:04 +0300

    add Question Model

---
+++ Millionaire/NetworkManager/QuestionModel.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionModel.swift
+//  Millionaire
+//
+//  Created by Marat Fakhrizhanov on 21.07.2025.
+//
+
+import Foundation
+
+struct Questions: Codable {
+    let results: [Question]
+}
+
+struct Question: Codable {
+    let type: String
+    let difficulty: String
+    let category: String
+    let question: String
+    let correctAnswer: String
+    let incorrectAnswers: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case type
+        case difficulty
+        case category
+        case question
+        case correctAnswer = "correct_answer"
+        case incorrectAnswers = "incorrect_answers"
+    }
+}
