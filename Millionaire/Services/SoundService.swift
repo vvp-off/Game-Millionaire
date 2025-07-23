b73588b5838b9538e3667271805a1cb86a56b084
Kirill Bakharovsky <baharovsky@bk.ru>
2025-07-24 00:02:10 +0500

    Complete SoundService

--- Millionaire/Services/SoundService.swift
+++ Millionaire/Services/SoundService.swift
@@ -5,8 +5,38 @@
 //  Created by Кирилл Бахаровский on 7/23/25.
 //
 
-import Foundation
+import AVFoundation
+
+enum GameSound: String {
+    case wrongAnswer = "OtvetNepravilnyiy"
+    case choiseIsMade = "OtvetPrinyat"
+    case correctAnswer = "OtvetVernyiy"
+    case timeTicking = "ZvukChasov"
+    case gameWin = "ZvukPobedu"
+}
 
 class SoundService {
+    private var audioPlayer: AVAudioPlayer?
     
+    func play(sound: GameSound) {
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: "mp3") else {
+            print("Sound is not found: \(sound.rawValue).mp3")
+            return
+        }
+        
+        do {
+            audioPlayer?.stop()
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.prepareToPlay()
+            audioPlayer?.play()
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func stop() {
+        audioPlayer?.stop()
+        audioPlayer = nil
+    }
 }
